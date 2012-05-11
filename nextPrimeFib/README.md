66ab1c2bd94a0cf0d31dee15734b5a72c7b71191
Matthew Drouin <matt@tabcreator.com>
2012-05-10 22:32:03 -0400

    Adding my first shot at figuring out the next prime fib greater than a given number

---
+++ nextPrimeFib/README.md
@@ -0,0 +1,2 @@
+# Programming Challenge: First prime fibonacci number larger than a given minimum (The Greplin Programming Challenge: level 2)
+
---
+++ nextPrimeFib/next_prime_fib.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'prime'
+
+def nextFib(n)
+  curr = 0
+  succ = 1
+  
+  until succ.prime? and succ > n
+    curr, succ = succ, curr + succ
+  end
+  
+  return succ
+end
+
+if ARGV.count == 1
+  puts nextFib(ARGV[0].to_i)
+else 
+  puts "USAGE: next_prime_fib.rb number"
+end
